e2dee9e3c82b65c083e91d40e6314cc408951668
heveryplus <Hevery_123@163.com>
2022-10-09 10:53:56 +0800

    使用axios调用后端接口

---
+++ src/main/java/com/hevery/wiki/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.hevery.wiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description: description
+ * @author: hevery
+ * @date: 2022/10/9
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600); // 一小时内不再预检 (发OPTION请求)
+    }
+}
