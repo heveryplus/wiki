698c99bc429831424998545c750c33f5b0fe9e7f
heveryplus <Hevery_123@163.com>
2022-10-09 11:38:46 +0800

    集成图标组件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -48,10 +48,38 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-  {{ ebooks }}
-  {{ ebooks2 }}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
 
     </a-layout-content>
   </a-layout>
@@ -61,28 +89,59 @@
 import { defineComponent, onMounted, ref, reactive, toRef} from 'vue';
 import axios from "axios";
 
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+
 export default defineComponent({
   name: 'Home',
   setup () {
     console.log("setup")
-    // ref 响应式数据
-    const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
-    // 生命周期钩子函数
-    onMounted(() => {
-      console.log("onMounted");
-      axios.get("http://localhost:8880/ebook/list?name=Spring").then(response => {
-        const data = response.data;
-        ebooks.value  = data.content;
-        ebooks1.books  = data.content;
-        console.log(response)
-      });
-    })
-    console.log(ebooks)
+    // // ref 响应式数据
+    // const ebooks = ref();
+    // const ebooks1 = reactive({books:[]});
+    // // 生命周期钩子函数
+    // onMounted(() => {
+    //   console.log("onMounted");
+    //   axios.get("http://localhost:8880/ebook/list?name=Spring").then(response => {
+    //     const data = response.data;
+    //     ebooks.value  = data.content;
+    //     ebooks1.books  = data.content;
+    //     console.log(response)
+    //   });
+    // })
+    // console.log(ebooks)
+    // return {
+    //   ebooks,
+    //   ebooks2: toRef(ebooks1, "books")
+    // }
+
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 3,
+    };
+    const actions: Record<string, string>[] = [
+      { type: 'StarOutlined', text: '156' },
+      { type: 'LikeOutlined', text: '156' },
+      { type: 'MessageOutlined', text: '2' },
+    ];
     return {
-      ebooks,
-      ebooks2: toRef(ebooks1, "books")
-    }
+      listData,
+      pagination,
+      actions,
+    };
   }
 });
 </script>
